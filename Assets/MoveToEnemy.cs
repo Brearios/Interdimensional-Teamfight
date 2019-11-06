af37fabc46a22cf128e035ade050f8220530e5da
Aaron <presin16@gmail.com>
2019-11-05 22:45:01 -0600

    Added color to shapes, created empty movement script

---
+++ Assets/MoveToEnemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Select or update Target
+        // If Distance > Range, Move toward that Target, Else Attack
+    }
+}
