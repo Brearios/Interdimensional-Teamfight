8dd14c44051d9137ef20267aa33d40afa6ef4a32
Aaron <presin16@gmail.com>
2019-11-10 00:01:21 -0600

    Selecting the nearest target of the other color
    Shapes select and move toward the nearest target of the opposite color. Can't seem to get the code right to make it "if outside of range", so that is commented out.

---
+++ Assets/Scripts/MoveTowardRed.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardRed : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start() { }
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceToClosestRed = Mathf.Infinity;
+        Red closestRed = null;
+        Red[] allReds = GameObject.FindObjectsOfType<Red>();
+
+        foreach (Red currentRed in allReds)
+        {
+            float distanceToRed = (currentRed.transform.position - transform.position).sqrMagnitude;
+            if (distanceToRed < distanceToClosestRed)
+            {
+                distanceToClosestRed = distanceToRed;
+                closestRed = currentRed;
+            }
+        }
+
+        // while (Vector3.Distance(closestRed.transform.position - transform.position) > gameObject.GetComponent.<Actor>.range
+        {
+            transform.position = Vector2.MoveTowards(transform.position, closestRed.transform.position, speed * Time.deltaTime);
+        }
+    }
+}
