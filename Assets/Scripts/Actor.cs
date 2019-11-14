03ee77bda4812e37ef8e37625bd1c2df5e4940e9
Aaron <presin16@gmail.com>
2019-11-13 23:49:43 -0600

    Broke movement by adding enums
    I'm not able to get the Team enums to compare like I'd hoped in the Update portion of Actor.CS

--- Assets/Scripts/Actor.cs
+++ Assets/Scripts/Actor.cs
@@ -9,6 +9,54 @@ public class Actor : MonoBehaviour
     // Later make this a random amount within a range
     public int globalCooldown;
     public string role;
-    public string team;
     public float range;
+    public float speed;
+    public enum Team { Blue, Red }; // Green, Purple, Orange
+    public enum State { Idle, Moving, Attacking };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Team myTeam;
+
+        State myState;
+
+        myState = State.Idle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.Instance.IsRunning == false)
+        {
+            return;
+        }
+        else
+        {
+            float distanceToClosestActor = Mathf.Infinity;
+            Actor closestActor = null;
+            Actor[] allActors = GameObject.FindObjectsOfType<Actor>();
+
+            foreach (Actor currentActor in allActors)
+            {
+                if (currentActor.Team == this.Team) // I can't get these to compare - have tried Actor.Team, this.Team, and other formats
+                {
+                    break;
+                }
+                else
+                {
+                    float distanceToActor = (currentActor.transform.position - transform.position).sqrMagnitude;
+                    if (distanceToActor < distanceToClosestActor)
+                    {
+                        distanceToClosestActor = distanceToActor;
+                        closestActor = currentActor;
+                    }
+                }
+
+                {
+                    transform.position = Vector2.MoveTowards(transform.position, closestActor.transform.position, speed * Time.deltaTime);
+                }
+            }
+        }
+    }
 }
